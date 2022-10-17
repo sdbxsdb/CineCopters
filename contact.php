2ccebae5e9c44f2bece2555b0f19f4f1afea3e04
sdbxsdb <69519326+sdbxsdb@users.noreply.github.com>
2022-10-17 11:35:07 +0100

    update phone numbers

--- contact.php
+++ contact.php
@@ -1,4 +1,3 @@
-
 <?php include("phpIncludes/contactMeta.php") ?>
 <?php include("phpIncludes/header.php") ?>
 <?php include("phpIncludes/nav.php") ?>
@@ -7,46 +6,46 @@
 <section id="contact">
 
   <div class="imgContainer">
-        <div class=bgImg>
-          <div id="title">
-            <h1>GET  IN  TOUCH</h1>
-          </div>
-            <div class="aboutText">
-              <p>If you have any questions about -</p>
-              <ul>
-                <li>Equipment or Additional Services</li>
-                <li>Technical Specs</li>
-                <li>Feasibility of an Idea</li>
-                <li>Stock Footage</li>
-              </ul>
-              <p> - or would like to book a CineCopters crew, send us an email or give us a call. <br><br> We'd be delighted to hear from you!  </p>
-              <div id="contactBounceBtn" class="arrow bounce"><i class="fa fa-angle-down fa-3x" aria-hidden="true"></i></div>
-          </div>
-        </div>
+    <div class=bgImg>
+      <div id="title">
+        <h1>GET  IN  TOUCH</h1>
+      </div>
+      <div class="aboutText">
+        <p>If you have any questions about -</p>
+        <ul>
+          <li>Equipment or Additional Services</li>
+          <li>Technical Specs</li>
+          <li>Feasibility of an Idea</li>
+          <li>Stock Footage</li>
+        </ul>
+        <p> - or would like to book a CineCopters crew, send us an email or give us a call. <br><br> We'd be delighted to hear from you! </p>
+        <div id="contactBounceBtn" class="arrow bounce"><i class="fa fa-angle-down fa-3x" aria-hidden="true"></i></div>
+      </div>
+    </div>
   </div>
 
 
   <div class="container" id="contactDetails">
     <div class="row">
-      
-      <div class="col">
-      <i class="fas fa-paper-plane"></i>
-      </div>  
 
-        <div class="col">
+      <div class="col">
+        <i class="fas fa-paper-plane"></i>
+      </div>
+
+      <div class="col">
         <i class="fas fa-mobile-alt"></i>
-        </div>
+      </div>
     </div>
 
     <div class="row ">
       <div class="col-md">
-      <h4>Bookings &amp; Enquiries</h4>
+        <h4>Bookings &amp; Enquiries</h4>
         <a href="mailto:info@cinecopters.co.uk?subject=Drone Filming Services">
-        <cite>info@cinecopters.co.uk</cite>
+          <cite>info@cinecopters.co.uk</cite>
         </a>
         <h4 id="accounts">Accounts</h4>
         <a href="mailto:accounts@cinemedia.ie?subject=Drone Filming Accounts Enquiry">
-        <cite>accounts@cinemedia.ie</cite>
+          <cite>accounts@cinemedia.ie</cite>
         </a>
       </div>
 
@@ -56,18 +55,18 @@
           Belfast |
           <a href="tel:00442890448286"> +4428 90 4482 86<a>
               <br><br>
-              Dublin | 
+              Dublin |
               <a href="tel:0035314869770">+353 1486 9770</a>
               <!-- <br><br>
               Glasgow | 
               <a href="tel:00441414590259">+44 141 459 0259</a> -->
               <!-- <br><br> -->
               <hr>
-              Technical  | 
-              <a href="tel:00447540251374">+4475 402 51374</a>
-              <br><br>
-              Technical  | 
+              Technical |
               <a href="tel:00447780586444">+4477 805 86444</a>
+              <br><br>
+              Technical |
+              <a href="tel:00447540251374">+4475 402 51374</a>
         </cite>
       </div>
     </div>
@@ -77,7 +76,7 @@
   <form id="contactForm" class="container" method="post" action="contactThanks.php">
     <h3>Contact Us</h3>
     <div class="row">
-      
+
       <div class="col-md">
         <div class="form-group">
           <input id="nameInput" type="text" name="name" onkeyup="manage(this)" class="form-control" placeholder="Name *" value="" />
@@ -100,10 +99,10 @@
       </div>
   </form>
 
-<div id="contactSendBtn" class="container">
+  <div id="contactSendBtn" class="container">
 
-<input id="contactSendBtnBtn" type="submit" name="btnSubmit" disabled class="btn btn-success" value="Send Message" />
-</div>
+    <input id="contactSendBtnBtn" type="submit" name="btnSubmit" disabled class="btn btn-success" value="Send Message" />
+  </div>
 
 
 
@@ -114,4 +113,4 @@
 </section>
 
 
-<?php include ("phpIncludes/footer.php") ?>
+<?php include("phpIncludes/footer.php") ?>
