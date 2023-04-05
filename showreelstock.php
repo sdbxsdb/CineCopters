c08f5fb1628cb61c3b232ea4e8cdf4b779debcc7
Sam <samdb@nisos.com>
2023-04-05 13:39:43 +0100

    updating F1 video and image

--- showreelstock.php
+++ showreelstock.php
@@ -144,7 +144,7 @@
               <div class="modal-body mb-0 p-0">
 
                 <div class="embed-responsive embed-responsive-16by9 z-depth-1-half">
-                  <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/e7ahtFFmUxY?modestbranding=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+                  <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/nsQBcxuuotg?modestbranding=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
                   </iframe>
                 </div>
               </div>
