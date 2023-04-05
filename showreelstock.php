5c593520c1769701b34306dd0d5e4cbc5efef613
Sam <samdb@nisos.com>
2023-04-05 12:19:55 +0100

    updated new reel for ready set action

--- showreelstock.php
+++ showreelstock.php
@@ -114,7 +114,7 @@
               <div class="modal-body mb-0 p-0">
 
                 <div class="embed-responsive embed-responsive-16by9 z-depth-1-half">
-                  <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/oBaoUjsrmZM?modestbranding=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+                  <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/7B61jPXpLyo?modestbranding=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
                   </iframe>
                 </div>
               </div>
